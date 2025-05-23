807be796fd3f2207580de1e58d480ea435cc8054
andi <a.sachs@vsd.volley.de>
2025-05-23 23:35:25 +0200

    New

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ then
   cp scoreboard-hardware-settings-temp/matrix.sh /home/pi/scoreboard-hardware-settings
 fi
 
-sudo cp /home/pi/scoreboard-hardware-settings/matrix.service /etc/systemd/system/
+sudo cp /home/pi/scoreboard-hardware-settings-temp/matrix.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable matrix.service

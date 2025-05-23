08fc895e11692222e233430333e2cc400e635120
andi <a.sachs@vsd.volley.de>
2025-05-23 21:13:58 +0200

    New

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir -p /home/pi/scoreboard-hardware-settings
-cp demo.sh /home/pi/scoreboard-hardware-settings
+cp scoreboard-hardware-settings-temp/demo.sh /home/pi/scoreboard-hardware-settings
 
 if [ ! -f /home/pi/scoreboard-hardware-settings/matrix.sh ]
 then
-  cp matrix.sh /home/pi/scoreboard-hardware-settings
+  cp scoreboard-hardware-settings-temp/matrix.sh /home/pi/scoreboard-hardware-settings
 fi

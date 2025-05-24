da959324f7aa3081405b4193477248d55d8233f1
andi <a.sachs@vsd.volley.de>
2025-05-24 11:06:08 +0200

    New

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ mkdir -p /home/pi/scoreboard-hardware-settings
 cp scoreboard-hardware-settings-temp/demo.sh /home/pi/scoreboard-hardware-settings
 cp -r scoreboard-hardware-settings-temp/rgb /home/pi/scoreboard-hardware-settings
 chmod 755 /home/pi/scoreboard-hardware-settings/demo.sh
+chmod 755 /home/pi/scoreboard-hardware-settings/rgb.matrix.py
 if [ ! -f /home/pi/scoreboard-hardware-settings/matrix.sh ]
 then
   cp scoreboard-hardware-settings-temp/matrix.sh /home/pi/scoreboard-hardware-settings

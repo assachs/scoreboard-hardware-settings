55d7af8aef0105b60354d9853331f10cc817aa6f
andi <a.sachs@vsd.volley.de>
2025-05-29 00:03:20 +0200

    New

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ ! -f /home/pi/scoreboard-hardware-settings/matrix.sh ]
 then
   cp scoreboard-hardware-settings-temp/matrix.sh /home/pi/scoreboard-hardware-settings
 fi
-
+sudo apt install python3-netifaces
 sudo cp /home/pi/scoreboard-hardware-settings-temp/matrix.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable matrix.service

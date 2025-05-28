5d0b5738085d136843c8853fa2129bf052ba7ef7
andi <a.sachs@vsd.volley.de>
2025-05-29 00:04:42 +0200

    New

--- install.sh
+++ install.sh
@@ -11,3 +11,18 @@ sudo apt install python3-netifaces
 sudo cp /home/pi/scoreboard-hardware-settings-temp/matrix.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable matrix.service
+
+cd ~/rpi-rgb-led-matrix/fonts/
+
+otf2bdf -v -o FreeSans-36-20.bdf -r 36 -p 20 /usr/share/fonts/opentype/freefont/FreeSans.otf
+otf2bdf -v -o FreeSans-72-30.bdf -r 72 -p 30 /usr/share/fonts/opentype/freefont/FreeSans.otf
+otf2bdf -v -o FreeSans-72-66.bdf -r 72 -p 66 /usr/share/fonts/opentype/freefont/FreeSans.otf
+otf2bdf -v -o FreeSans-72-66.bold.bdf -r 72 -p 66 /usr/share/fonts/opentype/freefont/FreeSansBold.otf
+
+ln -s FreeSans-72-30.bdf saetze.bdf
+ln -s FreeSans-72-66.bold.bdf punkte.bdf
+ln -s FreeSans-72-66.bdf doppelpunkt.bdf
+ln -s FreeSans-36-20.bdf teamnamen.bdf
+ln -s FreeSans-36-20.bdf auszeit.bdf
+
+cd ~
